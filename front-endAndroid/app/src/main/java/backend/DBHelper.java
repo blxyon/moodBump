d3b54cbee0a56ab88b0de5858bb03c26fe69b80e
Ilja Iskovs <ii263@bath.ac.uk>
2021-04-01 20:42:04 +0100

    add a function to get journal entries for specific date

--- front-endAndroid/app/src/main/java/backend/DBHelper.java
+++ front-endAndroid/app/src/main/java/backend/DBHelper.java
@@ -5,10 +5,16 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Build;
 import android.util.Log;
 
+import androidx.annotation.RequiresApi;
+
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 
 public class DBHelper extends SQLiteOpenHelper {
@@ -49,7 +55,7 @@ public class DBHelper extends SQLiteOpenHelper {
         ContentValues val = new ContentValues();
         val.put("text", entry.getText());
         val.put("mood", entry.getMood());
-        val.put("date", entry.getDate().getTime());
+        val.put("date", entry.getDate().getTime()/1000);
         if (db.insert(DATABASE_TABLE_ENTRIES, null, val) == -1) {
             Log.e("DataBase", "Could not insert a new row");
         }
@@ -64,14 +70,46 @@ public class DBHelper extends SQLiteOpenHelper {
             Log.e("DataBase", "Could not insert a new row");
         }
     }
-    
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public ArrayList<journalEntry> getEntryByDate(Date date) {
+        GregorianCalendar c = new GregorianCalendar();
+        c.setTime(date);
+        c.set(Calendar.HOUR_OF_DAY, 0);
+        c.set(Calendar.MINUTE, 0);
+        c.set(Calendar.SECOND, 0);
+        c.set(Calendar.MILLISECOND, 0);
+        int start = (int) (c.getTime().getTime()/1000);
+        c.set(Calendar.HOUR_OF_DAY, 23);
+        c.set(Calendar.MINUTE, 59);
+        c.set(Calendar.SECOND, 59);
+        c.set(Calendar.MILLISECOND, 999);
+        int end = (int) (c.getTime().getTime()/1000);
+
+        String sql = "SELECT * from " + DATABASE_TABLE_ENTRIES +
+                "WHERE date >= date(" + Integer.toString(start) + ")" +
+                "AND date < date(" + Integer.toString(end) + ")";
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.rawQuery(sql);
+        ArrayList<journalEntry> entries = new ArrayList<journalEntry>();
+        res.moveToFirst();
+        while(res.isAfterLast() == false) {
+            Date d = new Date(res.getLong(res.getColumnIndex("date"))*1000);
+            String text = res.getString(res.getColumnIndex("text"));
+            float mood = res.getFloat(res.getColumnIndex("mood"));
+            entries.add(new journalEntry(d, text, mood));
+            res.moveToNext();
+        }
+        return entries;
+    }
+
     public ArrayList<journalEntry> getAllEntries() {
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor res = db.rawQuery("select * from " + DATABASE_TABLE_ENTRIES, null);
         res.moveToFirst();
         ArrayList<journalEntry> entries = new ArrayList<journalEntry>();
         while (res.isAfterLast() == false) {
-            Date date = new Date(res.getLong(res.getColumnIndex("date")));
+            Date date = new Date(res.getLong(res.getColumnIndex("date"))*1000);
             String text = res.getString(res.getColumnIndex("text"));
             float mood = res.getFloat(res.getColumnIndex("mood"));
             entries.add(new journalEntry(date, text, mood));
