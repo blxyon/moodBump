f8e6dab230f32269dcbe94657a5460531db5345e
Ilja Iskovs <ii263@bath.ac.uk>
2021-04-02 16:47:05 +0100

    make a simple mood request handler

--- server/moodBump/moodBump/urls.py
+++ server/moodBump/moodBump/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from moodbumpAPI import views
 
 urlpatterns = [
+    path('mood/', views.mood),
     path('admin/', admin.site.urls),
 ]
--- server/moodBump/moodbumpAPI/views.py
+++ server/moodBump/moodbumpAPI/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def mood(request):
+    text = request.GET["text"]
+    print("Mood request with text \"{}\"".format(text))
+    #calculate the mood and send back the result
+    #echo the request for now
+    return HttpResponse(text)
