c53dcd92cfdde16610d721f94dc6b2ad6d1d9df9
Kai Patel <00kaipatel@gmail.com>
2021-04-04 20:01:08 +0100

    Basic sentiment analysis added

--- server/moodBump/moodbumpAPI/sentiment.py
+++ server/moodBump/moodbumpAPI/sentiment.py
@@ -1,6 +1,11 @@
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
 def analyse(data):
     text = data['text']
     print(text)
+    analyzer = SentimentIntensityAnalyzer()
+    vs = analyzer.polarity_scores(text)
+
     return {
-        "sentiment": "hi from your analysis function!"
+        "sentiment": vs
     }
