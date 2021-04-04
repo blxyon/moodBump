1415aec061fa0b036355104329f7688f19d457c5
Kai Patel <00kaipatel@gmail.com>
2021-04-04 16:10:30 +0100

    Added sentiment module for handling of actual analysis. Linked said module to sentiment endpoint view

---
+++ server/moodBump/moodbumpAPI/sentiment.py
@@ -0,0 +1,6 @@
+def analyse(data):
+    text = data['text']
+    print(text)
+    return {
+        "sentiment": "hi from your analysis function!"
+    }
--- server/moodBump/moodbumpAPI/views.py
+++ server/moodBump/moodbumpAPI/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+import os
+from moodbumpAPI.sentiment import analyse
 
-@api_view(['GET'])
+@api_view(['POST'])
 def analyse_sentiment(request):
-    return Response({"sentiment": "hello world!"})
+    sentiment = analyse(request.POST)
+    return Response(sentiment)
