799915bed692aea2fd3306cfefea4c238b5e344b
Lucy Green <lsg38@bath.ac.uk>
2021-04-05 14:31:45 +0100

    Updated sentiment analysis

--- server/moodBump/moodbumpAPI/sentiment.py
+++ server/moodBump/moodbumpAPI/sentiment.py
@@ -1,11 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[65]:
+
+
+# NLP library
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-def analyse(data):
-    text = data['text']
-    print(text)
-    analyzer = SentimentIntensityAnalyzer()
-    vs = analyzer.polarity_scores(text)
 
-    return {
-        "sentiment": vs
-    }
+# In[66]:
+
+
+def analyse(data):
+    #text = data['text']
+    #print(data)
+    analyzer = SentimentIntensityAnalyzer()
+    vs = analyzer.polarity_scores(data)
+    return vs
+
+
+# In[67]:
+
+
+def getValues(sentiment):
+    neg = sentiment['neg']
+    pos = sentiment['pos']
+    compound = sentiment['compound']
+    return neg, pos, compound
+
+
+# In[72]:
+
+
+def getView(sentiment):
+    compound = sentiment['compound']
+    
+    if compound >= 0.8:
+        view = "extremely positive"
+    elif compound < 0.8 and compound >= 0.5:
+        view = "very positive"
+    elif compound < 0.5 and compound >= 0.2:
+        view = "positive"
+    elif compound < 0.2 and compound >= -0.2:
+        view = "indifferent"
+    elif compound < -0.2 and compound >= -0.5:
+        view = "negative"
+    elif compound < -0.5 and compound >= -0.8:
+        view = "very negative"
+    elif compound < -0.8:
+        view = "extremely negative"
+        
+    return compound, view    
+
+
+# In[73]:
+
+
+# sentiment = analyse("I hate cats the most so much cats are vile")
+# neg, pos, compound = getValues(sentiment)
+# compound, view = getView(compound)
+# print(view, compound)
+
+
+# In[ ]:
+
+
+
+
--- server/moodBump/moodbumpAPI/views.py
+++ server/moodBump/moodbumpAPI/views.py
@@ -1,12 +1,45 @@
-from django.shortcuts import render
+#!/usr/bin/env python
+# coding: utf-8
 
-# Create your views here.
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-import os
-from moodbumpAPI.sentiment import analyse
+# In[45]:
 
-@api_view(['POST'])
-def analyse_sentiment(request):
-    sentiment = analyse(request.POST)
-    return Response(sentiment)
+
+import import_ipynb
+from sentiment import analyse
+from sentiment import getView
+from nltk.tokenize import sent_tokenize
+
+
+# In[56]:
+
+
+# Split text into sentances 
+text = "I feel like getting the mole on my back removed :("
+sentances = sent_tokenize(text)
+print(sentances)
+
+
+# In[57]:
+
+
+# For each sentance get the sentiment 
+views = [] # [compound, view]
+for i in range(len(sentances)):
+    sentiment = analyse(sentances[i])
+    compound, view = getView(sentiment)
+    views.append([getView(sentiment)])
+print(views)
+
+
+# In[58]:
+
+
+# Find mean of all sentances for overall mood 
+getView(analyse(text))
+
+
+# In[ ]:
+
+
+
+
